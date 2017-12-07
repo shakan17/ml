435c4b6ca3dd9659b504627cab553a4c01ed1b76
ShankarG <shakan@qti.qualcomm.com>
2017-12-07 17:25:34 +0530

    update matmul

--- tf/matrix.py
+++ tf/matrix.py
@@ -1,10 +1,19 @@
 import tensorflow as tf
 
-a=tf.constant([1,2,3,4,5,6,7,8,9])
-b=tf.constant([1,0,0,0,1,0,0,0,1])
-
+a=tf.constant([1,2,3,4,5,6,7,8,9],name="a",shape=(3,3))
+b=tf.constant([1,1,1,1,1,1,1,1,1],name="b",shape=(3,3))
+u=tf.Variable(tf.ones([3,3]))
 #use reshape to make diminsons
+
+#global varibale initialization
+init=tf.global_variables_initializer()
+
 c=tf.matmul(tf.reshape(a,[3,3]),tf.reshape(b,[3,3]))
 with tf.Session() as sess:
+	#global variables
+	sess.run(init)
+	print a
+	print b
+	print u.eval()
 	print sess.run(c)
 	
